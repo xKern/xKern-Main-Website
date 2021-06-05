9ef5a5436d24c86b54d70696f60b320d015d9365
arx8x <arx8@icloud.com>
2021-06-05 08:44:43 +0530

    disable header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
-import TheHeader from '@/components/the-header/TheHeader';
 import { useRouter } from 'next/router';
 
 import '../styles/base.scss';
 
 const xKernApp = ({ Component, pageProps }) => {
-  const router = useRouter();
 
   return (
     <main className="app">
-      <TheHeader fixed={router.pathname === '/'} />
       <Component {...pageProps} />
     </main>
   );
