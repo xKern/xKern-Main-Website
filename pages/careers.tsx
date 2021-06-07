de9109d69127f91f09f0662e624ca6d8be175fb2
arx8x <arx8@icloud.com>
2021-06-07 08:08:31 +0530

    add new openings in careers

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -9,42 +9,52 @@ const JobsPage = () => {
     {
       title: 'Solution Architect',
       description: 'Profound experience with technologies to architect our software solutions, plan and oversee the development our software',
-      url: '/jobs/jd/Job_Description_Architect.pdf'
+      url: '/files/jd/Job_Description_Architect.pdf'
     },
     {
       title: 'Senior iOS Developer',
       description: 'Proficiency in Swift, Xcode, instruments, storyboard, lldb and Cocoa Touch. Familiar with iOS design pattern, software technologies and development',
-      url: '/jobs/jd/Job_Description_iOS_Senior.pdf'
+      url: '/files/jd/Job_Description_iOS_Senior.pdf'
     },
     {
       title: 'Senior Android Developr',
       description: 'Proficiency in Java, KOTLIN, Android Studio, profiling tools, DDMS, ADB etc',
-      url: '/jobs/jd/Job_Description_Android_Senior.pdf'
+      url: '/files/jd/Job_Description_Android_Senior.pdf'
     },
     {
       title: 'Junior iOS Developer',
       description: 'Experience in Swift and Cocoa Touch to build the next generation iOS Applications',
-      url: '/jobs/jd/Job_Description_iOS_Junior.pdf'
+      url: '/files/jd/Job_Description_iOS_Junior.pdf'
     },
     {
       title: 'Junior Android Developer',
       description: 'Experience with Android platform, design language and application development to build the next generation apps',
-      url: '/jobs/jd/Job_Description_Android_Junior.pdf'
+      url: '/files/jd/Job_Description_Android_Junior.pdf'
     },
     {
       title: 'Web Developer',
       description: 'Good experience with HTML5, CSS3 and popular front end technologies like TypeScript, Vue, React, npm, jquery etc. ',
-      url: '/jobs/jd/Job_Description_Web.pdf'
+      url: '/files/jd/Job_Description_Web.pdf'
+    },
+    {
+      title: 'Backend Developer',
+      description: 'Versed in python, python frameworks (fastAPI, django etc), ML, REST, SOAP, sockets and other modern backend software technologies',
+      url: '/files/jd/Job_Description_Backend.pdf'
+    },
+    {
+      title: 'Marketing Executive',
+      description: 'Smart and energetic applicants with great communication and presentations skills and more than 1 year of experience',
+      url: ''
     },
     {
       title: 'Front Office and Administration',
       description: 'Excellent multilingual communication skills, friendly and cheerful mannerism, good administrative skills and fair experience with office software',
-      url: '/jobs/jd/Job_Description_Front_Office.pdf'
+      url: '/files/jd/Job_Description_Front_Office.pdf'
     },
     {
       title: 'Tester',
       description: 'Analyzing software that we build, considering use cases and testing the software to find flaws, inconsistencies and suggesting improvements',
-      url: '/jobs/jd/Job_Description_Tester.pdf'
+      url: '/files/jd/Job_Description_Tester.pdf'
     }
   ];
 
